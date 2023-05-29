53684910697aaf4b8a6e5972044931f928799969
Suleman Soren <chintutudu2@gmail.com>
2023-05-29 17:10:11 +0530

    Project setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,38 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'import-glob',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        cwd: 'babelrc',
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@app': './src/App',
+          '@api': './src/API',
+          '@assets': './src/Assets',
+          '@config': './src/Config',
+          '@constants': './src/Constants',
+          '@utils': './src/Utils',
+          '@rrematch': './src/Rematch',
+          '@components': './src/Components',
+          '@screens': './src/Screens',
+          '@interfaces': './src/Interfaces',
+          '@navigators': './src/Navigators',
+          '@helpers': './src/Helpers',
+          '@themes': './src/Themes',
+        },
+      },
+    ],
+    'jest-hoist',
+  ],
 };
---
+++ index.d.ts
@@ -0,0 +1,17 @@
+declare module '*.jpg';
+declare module '*.json';
+declare module '*.png';
+
+declare module '@app/*';
+declare module '@api/*';
+declare module '@assets/*';
+declare module '@config/*';
+declare module '@constants/*';
+declare module '@utils/*';
+declare module '@rrematch/*';
+declare module '@components/*';
+declare module '@screens/*';
+declare module '@interfaces/*';
+declare module '@navigators/*';
+declare module '@helpers/*';
+declare module '@themes/*';
